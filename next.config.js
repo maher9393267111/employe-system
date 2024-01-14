fd24195a405e4590f741ca4af33823e41c9e8a23
maher9393267111 <89878167+maher9393267111@users.noreply.github.com>
2024-01-14 13:23:58 +0300

    add babel loader

--- next.config.js
+++ next.config.js
@@ -8,4 +8,24 @@ module.exports = {
     theme: "DEFAULT",
     currency: "USD",
   },
+
+
+  webpack: (config, { dev }) => {
+    config.devtool = 'eval'
+    config.module.rules.push(
+      {
+        test: /\.(css|scss)/,
+        loader: 'emit-file-loader',
+        options: {
+          name: 'dist/[path][name].[ext]'
+        }
+      },
+      {
+        test: /\.css$/,
+        use: ['babel-loader', 'raw-loader', 'style-loader', 'css-loader']
+      }
+    )
+    return config
+  }
+
 };
