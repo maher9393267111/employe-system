1d61ad62a2df1abe147ac6e32d9084511980498f
maher9393267111 <89878167+maher9393267111@users.noreply.github.com>
2024-01-14 16:05:40 +0300

    add

--- next.config.js
+++ next.config.js
@@ -16,22 +16,30 @@ module.exports = {
 
 
 
-  webpack: (config, { dev }) => {
-    config.devtool = 'eval'
-    config.module.rules.push(
-      {
-        test: /\.(css|scss)/,
-        loader: 'emit-file-loader',
-        options: {
-          name: 'dist/[path][name].[ext]'
-        }
-      },
-      {
-        test: /\.css$/,
-        use: ['babel-loader', 'raw-loader', 'style-loader', 'css-loader']
-      }
-    )
-    return config
-  }
+  // webpack: (config, { dev }) => {
+  //   config.devtool = 'eval'
+  //   config.module.rules.push(
+  //     {
+  //          test: /\.(css|scss)/,
+  //        loader: 'emit-file-loader',
+  //       options: {
+  //         name: 'dist/[path][name].[ext]'
+  //       }
+  //     },
+  //     {
+  //          test: /\.css$/,
+  //       use: ['babel-loader',
+  //        'raw-loader', 
+  //         'style-loader', 
+  //         'css-loader'
+      
+  //     ]
+  //     }
+  //   )
+  //   return config
+  // }
+
+
+
 
 };
