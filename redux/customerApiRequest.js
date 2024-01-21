c9578511776d4e8ebddc6f0010b6265994cb1af9
maher9393267111 <89878167+maher9393267111@users.noreply.github.com>
2024-01-21 22:50:13 +0300

    add production url

--- redux/customerApiRequest.js
+++ redux/customerApiRequest.js
@@ -11,7 +11,7 @@ import { toast } from "react-toastify";
 
 const REACT_APP_BASE_URL1 = "https://clownfish-app-tzjmm.ondigitalocean.app";
 const REACT_APP_BASE_URL = "http://localhost:8000";
-const baseUrl = REACT_APP_BASE_URL
+const baseUrl = REACT_APP_BASE_URL1
   // process.env.NODE_ENV === "development"
   //   ? REACT_APP_BASE_URL
   //   : REACT_APP_BASE_URL1;
