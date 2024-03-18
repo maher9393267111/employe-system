dfcea5ef7b1cfcceb862559b17e8304a25b46d75
maher9393267111 <89878167+maher9393267111@users.noreply.github.com>
2024-03-18 23:21:23 +0300

    adddd

--- redux/baseURL.js
+++ redux/baseURL.js
@@ -4,4 +4,6 @@
 export const APIURL =  process.env.NODE_ENV === "development"?
 "http://localhost:3300"
 :
-"https://dolphin-app-lu45l.ondigitalocean.app"
+//"https://jellyfish-app-as8az.ondigitalocean.app/"
+
+"https://dolphin-app-lu45l.ondigitalocean.app/"
